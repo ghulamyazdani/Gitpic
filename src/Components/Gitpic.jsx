c11e28f89a575688a4ce32ec9062c51de592b6ec
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-12-14 14:25:37 +0530

    Gitpic setup

---
+++ src/Components/Gitpic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Gitpic.scss"
+
+export default function Gitpic() {
+    return (
+        <div>
+            <h1>Chal ra hai</h1>
+            
+        </div>
+    )
+}
