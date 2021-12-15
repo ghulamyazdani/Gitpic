d9e5aa0f577ddd2fbed00919d0f043e373513d1a
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-12-15 14:24:48 +0530

    dom-to-image improved

--- src/Components/Gitpic.jsx
+++ src/Components/Gitpic.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import html2canvas from "html2canvas";
+// import html2canvas from "html2canvas";
+import domtoimage from "dom-to-image";
 import "./Gitpic.scss";
 import Org from "./Org";
 export default function Gitpic() {
@@ -11,53 +12,62 @@ export default function Gitpic() {
   useEffect(() => {
     getUserData();
     getOrg();
-    setUpDownloadPageAsImage();
+    // setUpDownloadPageAsImage();
   }, []); //eslint-disable-line
-  
 
-function setUpDownloadPageAsImage() {
-  const el = document.getElementById("download-page-as-image");
-  const capture = document.getElementById("capture");
-  el.addEventListener("click", function() {
-    html2canvas(capture).then(function(canvas) {
-      console.log(canvas);
-      simulateDownloadImageClick(canvas.toDataURL(), 'file-name.png');
-    });
-  });
-}
+  // function setUpDownloadPageAsImage() {
+  //   const el = document.getElementById("download-page-as-image");
+  //   const capture = document.getElementById("capture");
+  //   el.addEventListener("click", function() {
+  //     html2canvas(capture).then(function(canvas) {
+  //       console.log(canvas);
+  //       simulateDownloadImageClick(canvas.toDataURL(), 'file-name.png');
+  //     });
+  //   });
+  // }
 
-function simulateDownloadImageClick(uri, filename) {
-  var link = document.createElement('a');
-  if (typeof link.download !== 'string') {
-    window.open(uri);
-  } else {
-    link.href = uri;
-    link.download = filename;
-    accountForFirefox(clickLink, link);
+  // function simulateDownloadImageClick(uri, filename) {
+  //   var link = document.createElement('a');
+  //   if (typeof link.download !== 'string') {
+  //     window.open(uri);
+  //   } else {
+  //     link.href = uri;
+  //     link.download = filename;
+  //     accountForFirefox(clickLink, link);
+  //   }
+  // }
+
+  // function clickLink(link) {
+  //   link.click();
+  // }
+
+  // function accountForFirefox(click) { // wrapper function
+  //   let link = arguments[1];
+  //   document.body.appendChild(link);
+  //   click(link);
+  //   document.body.removeChild(link);
+  // }
+  function shot() {
+    domtoimage
+      .toJpeg(document.getElementById("capture"), { quality: 0.95 })
+      .then(function (dataUrl) {
+        var link = document.createElement("a");
+        link.download = "my-image-name.Jpeg";
+        link.href = dataUrl;
+        link.click();
+      });
   }
-}
 
-function clickLink(link) {
-  link.click();
-}
-
-function accountForFirefox(click) { // wrapper function
-  let link = arguments[1];
-  document.body.appendChild(link);
-  click(link);
-  document.body.removeChild(link);
-}
-
-// fetching from api
+  // fetching from api
   async function getUserData() {
     const resp = await fetch(APIURL + username);
     const respData = await resp.json();
-    setData(respData?respData:[]);
+    setData(respData ? respData : []);
   }
   async function getOrg() {
     const resp = await fetch(APIURL + username + "/orgs");
     const respData = await resp.json();
-    setOrg(Array.isArray(respData)?respData:[]);
+    setOrg(Array.isArray(respData) ? respData : []);
   }
   const {
     name,
@@ -71,8 +81,11 @@ function accountForFirefox(click) { // wrapper function
   } = data;
 
   return (
-    <div>
-      <h1><i className="fab fa-git"></i><i className="fas fa-images"></i> </h1>
+    <div className="responsive">
+      <h1>
+        <i className="fab fa-git"></i>
+        <i className="fas fa-images"></i>{" "}
+      </h1>
       <form
         id="form"
         onSubmit={(e) => {
@@ -91,39 +104,53 @@ function accountForFirefox(click) { // wrapper function
         />
         <button className="button">Generate</button>
       </form>
-      <main id="main">
-        <div className="card" id="capture">
-          <div className="align">
-            <a href={html_url}>
-              <img src={avatar_url} alt="" />
-            </a>
-            <span className="company">{company}</span>
-            <div className="follow-stats">
-              <span>
-                {followers}
-                <b>Followers</b>
-              </span>
-              <span>
-                {following}
-                <b>Following</b>
-              </span>
-              <span>
-                {public_repos}
-                <b>Repos</b>
-              </span>
+      <div id="capture">
+        <main id="main">
+          <div className="card">
+            <div className="align">
+              <a href={html_url}>
+                <img src={avatar_url} alt="" />
+              </a>
+              <span className="company">{company}</span>
+              <div className="follow-stats">
+                <span>
+                  {followers}
+                  <b>Followers</b>
+                </span>
+                <span>
+                  {following}
+                  <b>Following</b>
+                </span>
+                <span>
+                  {public_repos}
+                  <b>Repos</b>
+                </span>
+              </div>
+            </div>
+            <div className="info-container">
+              <h3>{name}</h3>
+              <p>{bio}</p>
+              <h5>Part of:</h5>
+              <div id="orgs" className="orgs">
+                {org.length > 0 ? (
+                  <Org organisations={org} />
+                ) : (
+                  <p>not part of any organisation</p>
+                )}
+              </div>
             </div>
           </div>
-          <div className="info-container">
-            <h3>{name}</h3>
-            <p>{bio}</p>
-            <h5>Part of:</h5>
-            <div id="orgs" className="orgs">
-              {org.length>0?<Org organisations={org} />:<p>not part of any organisation</p>}
-            </div>
-          </div>
-        </div>
-        <button id="download-page-as-image">Download Page as Image</button>
-      </main>
+         
+        </main>
+      </div>
+      <button
+            onClick={() => {
+              shot();
+            }}
+            id="download-page-as-image"
+          >
+            Download Page as Image
+          </button>
     </div>
   );
 }
--- src/Components/Org.jsx
+++ src/Components/Org.jsx
@@ -1,19 +1,18 @@
- import React from 'react'
- 
- export default function Org({organisations}) {
-     return (<>
-             {organisations?organisations.map((org) => (
-              <div className="org" key={org.avatar_url}>
-              <img src={org.avatar_url} alt="" />
-              <span>{org.login}</span>
-              </div>
-            )):<p>not part of any org</p>}
-            </>
-           
-     )
- }
- 
- 
- 
- 
-           
+import React from "react";
+
+export default function Org({ organisations }) {
+  return (
+    <>
+      {organisations ? (
+        organisations.map((org) => (
+          <div className="org" key={org.avatar_url}>
+            <img src={org.avatar_url} alt="" />
+            <span>{org.login}</span>
+          </div>
+        ))
+      ) : (
+        <p>not part of any org</p>
+      )}
+    </>
+  );
+}
